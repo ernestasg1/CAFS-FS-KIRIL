c3a78743caba26444a860a1a0cd9703d980675db
ernestasg1 <griskevicius.ernestas@gmail.com>
2022-12-06 19:52:01 +0200

    task1 clean

--- 2022.12.06/index.js
+++ 2022.12.06/index.js
@@ -1,15 +1,15 @@
 let bandMembers = 0;
 let answer = '';
-if (bandMembers > 4){
+if (bandMembers > 4) {
     answer = 'didelė grupė';
 } else if (bandMembers > 3) {
    answer = 'kvartetas';
-}else if (bandMembers > 2){
+} else if (bandMembers > 2) {
     answer = 'trio';
-}else if (bandMembers > 1){
+} else if (bandMembers > 1) {
     answer = 'duetas';
-}else if (bandMembers > 0){
-    answer = 'solo'
+} else if (bandMembers > 0) {
+    answer = 'solo';
 } else {
     answer = 'ne grupė';
 }
