db9f36fb882b3585d265333bcc4badb433dc28c1
ernestasg1 <griskevicius.ernestas@gmail.com>
2023-01-10 20:22:56 +0200

    OS INFO

--- backEnd/src/task1.php
+++ backEnd/src/task1.php
@@ -1,6 +1,3 @@
 <?php
-// Naudojant PHP išveskite į naršyklę: "My name is {vardas}. My age is {amžius}"
-
-$name = 'Ernestas';
-$age = 27;
-echo "My name is {$name}. My age is {$age}";
+$osInfo= `cat /etc/os-release`;
+echo "OS INFO: $osInfo";
