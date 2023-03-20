621ffc72f5cec76adf031d137acaab7b4124cad1
ernestasg1 <griskevicius.ernestas@gmail.com>
2023-03-20 20:26:31 +0200

    newone

--- 2022.12.06/task3.js
+++
@@ -1,8 +0,0 @@
-// Parašykite „JavaScript“ funkciją, kuri grąžina perduotą eilutę su raidėmis abėcėlės tvarka.
- 
-
-function alphabetOrder(string){
-    return [...string].sort().join('');
-}
-
-console.assert(alphabetOrder('abca') == 'aabc');
--- backEnd/src/2023-02-08/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once ('Greetings.php');
-$greetings = new Greetings('Ernestas');
-$greetings->getGreetings();
-
